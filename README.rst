181c092117dd5a62dbbfeae90e713983b619cbd3
Gregor Boehl <admin@gregorboehl.com>
2022-03-01 22:38:59 +0100

    initialize repo

---
+++ README.rst
@@ -0,0 +1,7 @@
+
+emcwrap
+=======
+
+Collection of tools for Bayesian inference using `emcee <https://github.com/dfm/emcee>`_.
+
+
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from os import path
+
+# read the contents of the README file
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.rst"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/gboehl/emcwrap",
+    name="emcwrap",
+    version="0.1.0",
+    author="Gregor Boehl",
+    author_email="admin@gregorboehl.com",
+    description="Tools for Bayesian inference using Ensemble MCMC",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
+    include_package_data=True,
+    packages=find_packages(),
+    package_data={"pydsge": ["examples/*"]},
+    install_requires=[
+        "numpy",
+        "scipy",
+    ],
+)
