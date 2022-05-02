9432ca7632360afd9fda906c3ae993bff187747d
Gregor Boehl <admin@gregorboehl.com>
2022-05-02 10:39:34 +0200

    correct error handling

--- emcwrap/moves.py
+++ emcwrap/moves.py
@@ -18,16 +18,16 @@ class ADEMove(RedBlueMove):
     Args:
         sigma (float): The standard deviation of the Gaussian used to stretch
             the proposal vector.
-        gamma (Optional[float]): The mean stretch factor for the proposal
+        gamma0 (Optional[float]): The mean stretch factor for the proposal
             vector. By default, it is :math:`2.38 / \sqrt{2\,\mathrm{ndim}}`
             as recommended by the two references.
         threshold: (Optional[float]): the threshold proability for adapting an alternative walker. Should be _very_ low (e.g. `1e-32`).
         log_threshold: (Optional[float]): the threshold proability for adapting an alternative walker in log space. Should be _very_ low (e.g. `-100`).
     """
 
-    def __init__(self, sigma=1.0e-5, gamma=None, threshold=0, log_threshold=None, verbose=False, **kwargs):
+    def __init__(self, sigma=1.0e-5, gamma0=None, threshold=0, log_threshold=None, verbose=False, **kwargs):
         self.sigma = sigma
-        self.gamma = gamma
+        self.gamma0 = gamma0
         self.verbose = verbose
 
         if log_threshold and threshold:
@@ -40,10 +40,11 @@ class ADEMove(RedBlueMove):
         super(ADEMove, self).__init__(**kwargs)
 
     def setup(self, coords):
-        if self.gamma is None:
-            # not that much magic:
+        self.g0 = self.gamma0
+        if self.g0 is None:
+            # Pure MAGIC:
             ndim = coords.shape[1]
-            self.gamma = 2.38 / np.sqrt(2 * ndim)
+            self.g0 = 2.38 / np.sqrt(2 * ndim)
 
     def get_proposal(self, s, c, random):
         Ns = len(s)
@@ -54,7 +55,7 @@ class ADEMove(RedBlueMove):
         for i in range(Ns):
             w = np.array([c[j][random.randint(Nc[j])] for j in range(2)])
             random.shuffle(w)
-            g = np.diff(w, axis=0) * self.gamma + f[i]
+            g = np.diff(w, axis=0) * self.g0 + f[i]
             q[i] = s[i] + g
         return q, np.zeros(Ns, dtype=np.float64)
 
@@ -77,6 +78,7 @@ class ADEMove(RedBlueMove):
             )
 
         if not walkers_independent(state.coords):
+            print('hier! Problem!')
             raise ValueError(
                 "Current state has a large condition number. "
                 "Make sure that your walkers are linearly independent for the "
@@ -132,8 +134,8 @@ class ADEMove(RedBlueMove):
             new_state = State(q, log_prob=new_log_probs, blobs=new_blobs)
             state = self.update(state, new_state, accepted, S1)
 
-            if self.verbose and sum(adapt):
-                print(f'(ADEMove:) Accepted {sum(accepted[S1][adapt])} of {sum(adapt)} proposed adaptations in split {split}.')
+            # if self.verbose and sum(adapt):
+                # print(f'(ADEMove:) Accepted {sum(accepted[S1][adapt])} of {sum(adapt)} proposed adaptations in split {split}.')
 
         return state, accepted
 
@@ -180,3 +182,59 @@ class DEMove(RedBlueMove):
             g = np.diff(w, axis=0) * self.g0 + f[i]
             q[i] = s[i] + g
         return q, np.zeros(Ns, dtype=np.float64)
+
+    def propose(self, model, state):
+        """Use the move to generate a proposal and compute the acceptance
+
+        Args:
+            coords: The initial coordinates of the walkers.
+            log_probs: The initial log probabilities of the walkers.
+            log_prob_fn: A function that computes the log probabilities for a
+                subset of walkers.
+            random: A numpy-compatible random number state.
+
+        """
+        # Check that the dimensions are compatible.
+        nwalkers, ndim = state.coords.shape
+        if nwalkers < 2 * ndim and not self.live_dangerously:
+            raise RuntimeError(
+                "It is unadvisable to use a red-blue move "
+                "with fewer walkers than twice the number of "
+                "dimensions."
+            )
+
+        # Run any move-specific setup.
+        self.setup(state.coords)
+
+        # Split the ensemble in half and iterate over these two halves.
+        accepted = np.zeros(nwalkers, dtype=bool)
+        all_inds = np.arange(nwalkers)
+        inds = all_inds % self.nsplits
+        if self.randomize_split:
+            model.random.shuffle(inds)
+        for split in range(self.nsplits):
+            S1 = inds == split
+
+            # Get the two halves of the ensemble.
+            sets = [state.coords[inds == j] for j in range(self.nsplits)]
+            s = sets[split]
+            c = sets[:split] + sets[split + 1 :]
+
+            # Get the move-specific proposal.
+            q, factors = self.get_proposal(s, c, model.random)
+
+            # Compute the lnprobs of the proposed position.
+            new_log_probs, new_blobs = model.compute_log_prob_fn(q)
+
+            # Loop over the walkers and update them accordingly.
+            for i, (j, f, nlp) in enumerate(
+                zip(all_inds[S1], factors, new_log_probs)
+            ):
+                lnpdiff = f + nlp - state.log_prob[j]
+                if lnpdiff > np.log(model.random.rand()):
+                    accepted[j] = True
+
+            new_state = State(q, log_prob=new_log_probs, blobs=new_blobs)
+            state = self.update(state, new_state, accepted, S1)
+
+        return state, accepted
