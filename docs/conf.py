2dee3133e1bdf23575d2adae70b4bbdeea46820e
Gregor Boehl <admin@gregorboehl.com>
2022-05-29 14:33:07 +0200

    fix docs

--- docs/conf.py
+++ docs/conf.py
@@ -52,6 +52,8 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+# autodoc_mock_imports = ['econsieve', 'particles', 'numpy', 'pandas', 'emcee', 'kombine', 'pathos', 'yaml', 'sympy', 'scipy', 'numba', 'tqdm', 'dill', 'pygmo', 'matplotlib', 'interpolation', 'chaospy']
+autodoc_mock_imports = ['tqdm']
 
 html_theme_options = {
     "description": "Tools for Bayesian inference using Enseble MCMC",
